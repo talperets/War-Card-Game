b0d50ddfb7d5d6eb594d8a1983a3e6a85df318df
talperets <tal.perets3@gmail.com>
2023-04-19 23:56:30 +0300

    formatting Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,18 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 export default function Home() {
-    const [name, setName] = useState('')
+  const [name, setName] = useState("");
   return (
     <div>
-        <h1>Ready for War?</h1>
-        <input type="text" onChange={e=>{setName(e.target.value)}} placeholder='Enter Your name' />
-        <button disabled={name.length === 0? true: false}>Next</button>
+      <h1>Ready for War?</h1>
+      <input
+        type="text"
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+        placeholder="Enter Your name"
+      />
+      <button disabled={name.length === 0 ? true : false}>Next</button>
     </div>
-  )
+  );
 }
