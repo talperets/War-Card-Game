759e9564cc78184d690b8866a8acdaa4ea189885
talperets <tal.perets3@gmail.com>
2023-04-20 18:49:49 +0300

    finishing the Deck class

--- src/classes/Deck.js
+++ src/classes/Deck.js
@@ -1,5 +1,20 @@
 export default class Deck{
+    cards = []
 
+
+    initCards(){
+        for (let i = 1; i <= 13; i++) {
+            for (let j = 0; j < 4; j++) {
+                this.cards.push(i)
+            }
+            
+        }
+    }
+    dealCards(){
+        let randNum = Math.floor(Math.random() * this.cards.length)
+        let card = this.card.splice(randNum, 1)
+        return card[0]
+    }
 
     constructor(){
 
--- src/classes/Player.js
+++ src/classes/Player.js
@@ -1,7 +1,12 @@
 export default class Player{
+    wins = 0
+    losses = 0
+    games = 0
+    currentGame = ''
 
 
-    constructor(){
-
+    constructor(name, cards){
+        this.name = name
+        this.cards = cards
     }
 }
