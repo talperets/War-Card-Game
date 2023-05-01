9a92f0a3aa2a57f43d0b82703e88c042f79ed6c2
talperets <tal.perets3@gmail.com>
2023-05-01 15:06:52 +0300

    remaking build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://talperets.github.io/War-Card-Game/',
+  base: '/War-Card-Game',
   plugins: [react()],
 })
